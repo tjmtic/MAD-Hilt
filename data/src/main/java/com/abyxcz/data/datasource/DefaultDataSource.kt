6393a239bc6d2a391e806c02ddef1d828fb02f0c
TM <tjmcard@gmail.com>
2024-01-11 21:15:08 -0800

    Create DefaultDataSource

---
+++ data/src/main/java/com/abyxcz/data/datasource/DefaultDataSource.kt
@@ -0,0 +1,35 @@
+package com.abyxcz.data.datasource
+
+import com.abyxcz.data.entity.ItemEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Main entry point for accessing items data.
+ */
+interface DefaultDataSource {
+
+
+    fun observeItems(): Flow<Result<List<ItemEntity>>>
+
+    suspend fun getItems(): Result<List<ItemEntity>>
+
+    suspend fun refreshItems()
+
+    fun observeItem(itemId: String): Flow<Result<ItemEntity>>
+
+    suspend fun getItem(itemId: String): Result<ItemEntity>
+
+    suspend fun refreshItem(itemId: String)
+
+    suspend fun saveItem(item: ItemEntity)
+
+    suspend fun saveItems(items: List<ItemEntity>)
+
+    suspend fun updateItem(item: ItemEntity) : Int
+
+    suspend fun updateItems(items: List<ItemEntity>) : Int
+
+    suspend fun clearAllItems()
+
+    suspend fun clearItem(itemId: String)
+}
