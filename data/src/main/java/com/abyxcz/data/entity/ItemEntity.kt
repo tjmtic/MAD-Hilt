48e8058de10e773b41f02ee0b3b27243df38846b
TM <tjmcard@gmail.com>
2024-01-11 21:02:33 -0800

    Create ItemEntity.kt

---
+++ data/src/main/java/com/abyxcz/data/entity/ItemEntity.kt
@@ -0,0 +1,15 @@
+package com.abyxcz.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "items")
+data class ItemEntity(
+    @PrimaryKey(autoGenerate = true)
+    var pk: Long = 0,
+    @SerializedName("_id")
+    val id: String,
+    @SerializedName("id")
+    val itemId: String?
+)
