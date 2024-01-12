4cbe1e7975c4a94c80e0c1bb6af8bb7cafceb295
TM <tjmcard@gmail.com>
2024-01-11 22:50:00 -0800

    Update ItemEntity and Model

--- data/src/main/java/com/abyxcz/data/model/Item.kt
+++ data/src/main/java/com/abyxcz/data/model/Item.kt
@@ -6,31 +6,12 @@ import androidx.room.PrimaryKey
 import java.util.UUID
 
 
-/**
- * Immutable model class for a Item. In order to compile with Room, we can't use @JvmOverloads to
- * generate multiple constructors.
- *
- * @param title       title of the item
- * @param description description of the item
- * @param isCompleted whether or not this item is completed
- * @param id          id of the item
- */
-@Entity(tableName = "items")
+
 data class Item @JvmOverloads constructor(
-    @ColumnInfo(name = "title") var title: String = "",
-    @ColumnInfo(name = "description") var description: String = "",
-    @ColumnInfo(name = "completed") var isCompleted: Boolean = false,
-    //@ColumnInfo(name = "deleted") var isDeleted: Boolean = false,
-    @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
+    var itemId: String = "",
+    var title: String = "",
+    var description: String = "",
+    var isCompleted: Boolean = false,
 ) {
 
-    val titleForList: String
-        get() = if (title.isNotEmpty()) title else description
-
-
-    val isActive
-        get() = !isCompleted
-
-    val isEmpty
-        get() = title.isEmpty() || description.isEmpty()
 }
